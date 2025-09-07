57a491c9b018f6dc82febf2004c4815fae9b1cba
rhuanbrasil <rhuanbrazil@gmail.com>
2025-09-07 14:34:32 -0300

    feat :arrow_up: : essentials to run

---
+++ src/test/java/com/api/bankApi/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
