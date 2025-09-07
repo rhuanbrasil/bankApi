5b5fd9368203d8b6978d7ae29fd12dc643521d99
rhuanbrasil <rhuanbrazil@gmail.com>
2025-09-07 14:35:55 -0300

    feat :arrow_up: : enum for security roles

---
+++ src/main/java/com/api/bankApi/infra/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.api.bankApi.infra.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+}
