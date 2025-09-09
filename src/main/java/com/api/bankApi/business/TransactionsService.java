531acd689859b09b968531b8c093c5d606fa5886
rhuanbrasil <rhuanbrazil@gmail.com>
2025-09-09 13:28:28 -0300

    fix :arrow_up: : Refactoring the validations in the methods

--- src/main/java/com/api/bankApi/business/TransactionsService.java
+++ src/main/java/com/api/bankApi/business/TransactionsService.java
@@ -26,48 +26,74 @@ public class TransactionsService {
 
     @Transactional
     public TransactionResponseDto processDeposit(Transaction transaction){
-        Account destination = accRepo.findById(transaction.getDestinationAccountId();
+        Account destination = accRepo.findById(transaction.getDestinationAccountId());
         transaction.setAccount(destination);
-        if(transaction.getType() == TransactionEnums.DEPOSIT && transaction.getAccount() != null
-                && transaction.getAccount().getBalance() != null
-                && transaction.getAccount().getBalance().compareTo(BigDecimal.ZERO) >= 0 ){
+        if(transaction.getType() != TransactionEnums.DEPOSIT){
+            throw new TransactionExceptions("Tipo de transação inválido para deposito");
+        }
+        if(destination == null){
+              throw new TransactionExceptions("O número informado não corresponde a conta alguma!");
+          }
+        if(transaction.getAmount().compareTo(BigDecimal.ZERO) <= 0){
+            throw new TransactionExceptions("O valor de deposito deve ser maior que zero!");
+        }
                 destination.setBalance(destination.getBalance().add(transaction.getAmount()));
                 transaction.setTimestamp(LocalDateTime.now());
                 repo.save(transaction);
                 accRepo.save(destination);
                 return mapper.toTransactionResponseDto(transaction);
-        }
-        throw new TransactionExceptions("O id de destino não foi informado corretamente.");
     }
     @Transactional
     public TransactionResponseDto processWithdraw(Transaction transaction){
-        transaction.setAccount(accRepo.findById(transaction.getSourceAccountId()));
-        if(transaction.getType() == TransactionEnums.WITHDRAW && transaction.getAccount() != null
-           && transaction.getAccount().getBalance().compareTo(transaction.getAmount()) >= 0){
-            var currentBalance = transaction.getAccount().getBalance();
-            transaction.getAccount().setBalance(currentBalance.subtract(transaction.getAmount()));
-            transaction.setTimestamp(LocalDateTime.now());
-            repo.save(transaction);
-            accRepo.save(transaction.getAccount());
-            return mapper.toTransactionResponseDto(transaction);
+        Account sourceAccount = accRepo.findById(transaction.getSourceAccountId());
+        transaction.setAccount(sourceAccount);
+        if(transaction.getType() != TransactionEnums.WITHDRAW){
+            throw new TransactionExceptions("O tipo de transação não corresponde a operação de saque");
         }
-        throw new TransactionExceptions("O id informado não corresponde a nenhuma conta" +
-                                        " ou você não tem saldo o suficiente");
+        if (sourceAccount == null){
+            throw new TransactionExceptions("A conta informada não existe!");
+        }
+        if(sourceAccount.getBalance().compareTo(transaction.getAmount()) < 0){
+            throw new TransactionExceptions("Sua conta não tem saldo o suficiente");
+        }
+
+        var currentBalance = transaction.getAccount().getBalance();
+
+        transaction.getAccount().setBalance(currentBalance.subtract(transaction.getAmount()));
+        transaction.setTimestamp(LocalDateTime.now());
+
+        repo.save(transaction);
+        accRepo.save(transaction.getAccount());
+
+        return mapper.toTransactionResponseDto(transaction);
+
     }@Transactional
     public TransactionResponseDto processTransfer(Transaction transaction){
-        Account source = accRepo.findById(transaction.getSourceAccountId());
+        Account sourceAccount = accRepo.findById(transaction.getSourceAccountId());
         Account destinationAccount = accRepo.findById(transaction.getDestinationAccountId());
 
-        transaction.setAccount(accRepo.findById(transaction.getSourceAccountId()));
-        if(transaction.getType() == TransactionEnums.TRANSFER && source != null
-            && source.getBalance().compareTo(transaction.getAmount()) >= 0
-            && destinationAccount != null){
-            source.setBalance(source.getBalance().subtract(transaction.getAmount()));
-            destinationAccount.setBalance(destinationAccount.getBalance().add(transaction.getAmount()));
-            transaction.setTimestamp(LocalDateTime.now());
-            repo.save(transaction);
-            accRepo.save(source);
-            accRepo.save(destinationAccount);
+        transaction.setAccount(sourceAccount);
+
+        if(transaction.getType() != TransactionEnums.TRANSFER){
+            throw new TransactionExceptions("O tipo de transferencia não corresponde ao tipo de operação");
         }
+        if(sourceAccount == null){
+            throw new TransactionExceptions("A conta fonte informada não existe em nosso sistema");
+        }
+        if(sourceAccount.getBalance().compareTo(transaction.getAmount()) < 0){
+            throw new TransactionExceptions("Você não tem saldo o suficiente para realizar a transferência");
+        }
+        if(destinationAccount == null){
+            throw new TransactionExceptions("A conta de destino informada não existe em nosso sistema");
+        }
+
+        sourceAccount.setBalance(sourceAccount.getBalance().subtract(transaction.getAmount()));
+        destinationAccount.setBalance(destinationAccount.getBalance().add(transaction.getAmount()));
+        transaction.setTimestamp(LocalDateTime.now());
+
+        repo.save(transaction);
+        accRepo.save(sourceAccount);
+        accRepo.save(destinationAccount);
+        return mapper.toTransactionResponseDto(transaction);
     }
 }
