e9cb5aa88ee5eb8c179e64ba7f3ad31e82c70be2
rhuanbrasil <rhuanbrazil@gmail.com>
2025-09-09 13:29:08 -0300

    feat :arrow_up: : Starting Unit Tests

---
+++ src/test/java/com/api/bankApi/business/TransactionsServiceTest.java
@@ -0,0 +1,22 @@
+package com.api.bankApi.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class TransactionsServiceTest {
+
+    @Test
+    void processDeposit() {
+    }
+
+    @Test
+    void processWithdraw() {
+    }
+
+    @Test
+    void processTransfer() {
+    }
+}
